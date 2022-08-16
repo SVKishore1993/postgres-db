5109441e42f5a77a69102a14194b48043e4d448e
Venkata Kishore <venkata.kishore@searce.com>
2022-08-16 17:29:34 +0530

    updated

--- provider-psql.tf
+++ provider-psql.tf
@@ -1,5 +1,5 @@
 provider "postgresql" {
-  host            = "searce-sandbox-355505:australia-southeast1:postdatabase-instance"
+  host            = "searce-sandbox-355505:australia-southeast1:postdatabase-instance123"
   port            = 5432
   username        = "test"
   password        = "f40b75efed12fa5ff2a02b3fc60b611423c07dace377613a8b4532b56b4b511c"
